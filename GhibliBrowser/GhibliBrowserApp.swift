b0134109b2ca60e5e27a798cb47d8f4834881e93
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2024-07-26 16:58:12 -0500

    Initial Commit

---
+++ GhibliBrowser/GhibliBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliBrowserApp.swift
+//  GhibliBrowser
+//
+//  Created by user263110 on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GhibliBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
